d1d1bd2ea82d4987f67a16b160beb2128eac6ed7
Suducodes <sudarshanvasanthakumar@gmail.com>
2026-01-17 18:53:27 +0530

    Set base path for Vite configuration
    Added base path configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: "/PolyprimeV2.0/",  // <--- ADD THIS LINE (Make sure it matches your Repo name exactly)
+})
